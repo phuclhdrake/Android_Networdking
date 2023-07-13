77bb3409eea4d279e8654f70184b70ae2510f9fd
phucdrake <phucleaaee@gmail.com>
2023-07-13 14:19:39 +0700

    add lab02

---
+++ lab02/app/src/main/java/com/example/lab02/lab022/lab02Interface.java
@@ -0,0 +1,8 @@
+package com.example.lab02.lab022;
+
+import android.graphics.Bitmap;
+
+public interface lab02Interface {
+    void onLoadBitmap(Bitmap bitmap); //ham load anh
+    void onError(); // xu ly loi neu co
+}
